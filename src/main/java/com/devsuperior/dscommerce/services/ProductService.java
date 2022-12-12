9a2bb3b6b2e0516b7ac9bf6fdd1f9cd530ace314
Karoline Brito <karolineufcg@gmail.com>
2022-12-11 21:25:34 -0300

    PUT product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,8 +1,5 @@
 package com.devsuperior.dscommerce.services;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,18 +29,26 @@ public class ProductService {
 	}
 	
 	@Transactional
-	public ProductDTO insert(ProductDTO dto) {
-		
+	public ProductDTO insert(ProductDTO dto) {		
 		Product entity = new Product();
+		copyDtoToEntity(dto, entity);		
+		entity = repository.save(entity);		
+		return new ProductDTO(entity);		
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {	
+		Product entity = repository.getReferenceById(id);
+		copyDtoToEntity(dto, entity);		
+		entity = repository.save(entity);		
+		return new ProductDTO(entity);		
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
-		
-		entity = repository.save(entity);
-		
-		return new ProductDTO(entity);
-		
 	}
 
 
