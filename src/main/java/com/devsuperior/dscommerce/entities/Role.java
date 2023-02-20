bfdd40a130a2319cafa653089baf42b5029375c0
Karoline Brito <karolineufcg@gmail.com>
2023-02-20 11:02:12 -0300

    OAuth2 JWT Checklist

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -6,9 +6,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,7 +34,8 @@ public class Role {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
+	@Override
 	public String getAuthority() {
 		return authority;
 	}
