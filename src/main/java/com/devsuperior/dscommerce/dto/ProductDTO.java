2cbabbe9518d07dd14796ad8d046c6f3608d5dd7
Karoline Brito <karolineufcg@gmail.com>
2023-02-22 21:01:16 -0300

    Fix Validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,15 +1,16 @@
 package com.devsuperior.dscommerce.dto;
 
-import com.devsuperior.dscommerce.entities.Category;
-import com.devsuperior.dscommerce.entities.Product;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
+
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
 
 public class ProductDTO {
 	
@@ -23,6 +24,7 @@ public class ProductDTO {
 	@NotBlank(message = "Campo requerido")
 	private String description;
 	
+	@NotNull(message = "Campo requerido")
 	@Positive(message = "O price deve ser positivo")
 	private Double price;
 	private String imgUrl;
